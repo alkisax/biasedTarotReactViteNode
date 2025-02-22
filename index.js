96970a40b578c78fee61c167f5d2a50cbc1cea84
Alkis <alkisax@gmail.com>
2025-02-23 00:08:55 +0200

    basic func no user input still

--- index.js
+++ index.js
@@ -98,7 +98,9 @@ const errorHandler = (error, request, response, next) => {
 app.use(unknownEndpoint);
 // Add the error handler as the last middleware
 app.use(errorHandler);
-const PORT = process.env.PORT || 3000;
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
