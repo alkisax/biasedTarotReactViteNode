dfb613315a3b8b6b7d219c5ea0880650e3a9d2f4
Alkis <alkisax@gmail.com>
2025-02-23 10:50:29 +0200

    reschafle

--- GPTLogic.js
+++ GPTLogic.js
@@ -4,24 +4,25 @@ const { tarotDeck, draw } = require('./tarotLogic/tarotCards');
 tarot_prompt =  
   "You are a Tarot interpreter. Provide a detailed interpretation of the following Tarot cards in direct response to the question asked. For each card, describe its meaning thoroughly and explain its relevance to the question. After interpreting each card individually, provide a combined interpretation that synthesizes the meanings of all the cards in relation to the question asked. Avoid any introductory or contextual information, and focus solely on delivering a profound and insightful analysis of the individual cards and their combined significance. Do not refer to yourself or anything outside of the Tarot cards and their meanings."
 
-  // Draw three unique tarot cards
-const drawnCards = draw();
-
-console.log(`
-  draw:
-  first: ${tarotDeck[drawnCards.first]}
-  second: ${tarotDeck[drawnCards.second]}
-  third: ${tarotDeck[drawnCards.third]}
-  `);
-
-// Format the selected cards
-const selectedCards = [
-    tarotDeck[drawnCards.first],
-    tarotDeck[drawnCards.second],
-    tarotDeck[drawnCards.third]
-];
-
 const getTarotReading = (userQuestion, apiKey) => {
+
+  // Draw three unique tarot cards
+  const drawnCards = draw();
+
+  console.log(`
+    draw:
+    first: ${tarotDeck[drawnCards.first]}
+    second: ${tarotDeck[drawnCards.second]}
+    third: ${tarotDeck[drawnCards.third]}
+    `);
+
+  // Format the selected cards
+  const selectedCards = [
+      tarotDeck[drawnCards.first],
+      tarotDeck[drawnCards.second],
+      tarotDeck[drawnCards.third]
+  ];
+
   // Construct the full message for GPT-4
   const fullPrompt = `${tarot_prompt} 
   Question: ${userQuestion} 
